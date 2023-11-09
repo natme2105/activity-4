fdaac1ed102dbdffb5d19b0d142cf063bca8f95a
natme2105 <nathanielgutierrez70@gmail.com>
2023-11-10 02:12:26 +0800

    feat/create-table-Doctors

--- veterinary.sql
+++ veterinary.sql
@@ -25,3 +25,11 @@ CREATE TABLE animals(
     appointdate DATE,
     reason VARCHAR(255),
     FOREIGN KEY (animalid) REFERENCES Animals(animalid)
+CREATE TABLE Doctors (
+    doctorid INT PRIMARY KEY,
+    dfirstname VARCHAR(50),
+    dlastname VARCHAR(50),
+    speciality VARCHAR(100),
+    phone VARCHAR(15),
+    email VARCHAR(100)
+);
