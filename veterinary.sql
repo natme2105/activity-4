ac12a91bf49d807486ffadcbbadbed764d637748
natme2105 <nathanielgutierrez70@gmail.com>
2023-11-10 12:42:47 +0800

    feat/create-table-animals

--- veterinary.sql
+++ veterinary.sql
@@ -1,14 +1,5 @@
 CREATE DATABASE veterinary;
 
-CREATE TABLE owners( 
-    ownerid INT PRIMARY KEY, 
-    ofirstname VARCHAR(50) NOT NULL,
-    phone VARCHAR(15) NOT NULL, 
-    phone VARCHAR(15) NOT NULL,
-    olastname VARCHAR(50) NOT NULL, 
-    address VARCHAR(15) NOT NULL, 
-    email VARCHAR(100) NOT NULL );
-
 CREATE TABLE animals(
    animalid INT PRIMARY KEY,
    name VARCHAR(50) NOT NULL,
@@ -18,3 +9,4 @@ CREATE TABLE animals(
    gender VARCHAR(10) NOT NULL,
    color VARCHAR(50) NOT NULL,
    ownerid INT,foreign key (ownerid) references owners(ownerid));
+   
