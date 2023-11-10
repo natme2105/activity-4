07844bcbc14c274302ba5a2d1628ee7f14eae7aa
natme2105 <nathanielgutierrez70@gmail.com>
2023-11-10 13:28:20 +0800

    feat/create-table-MedicalRecords

--- veterinary.sql
+++ veterinary.sql
@@ -1,44 +1,5 @@
 CREATE DATABASE veterinary;
 
-CREATE TABLE owners( 
-    ownerid INT PRIMARY KEY, 
-    ofirstname VARCHAR(50) NOT NULL,
-    phone VARCHAR(15) NOT NULL, 
-    phone VARCHAR(15) NOT NULL,
-    olastname VARCHAR(50) NOT NULL, 
-    address VARCHAR(15) NOT NULL, 
-    email VARCHAR(100) NOT NULL );
-
-CREATE TABLE animals(
-   animalid INT PRIMARY KEY,
-   name VARCHAR(50) NOT NULL,
-   species VARCHAR(50) NOT NULL,
-   breed VARCHAR(50) NOT NULL,
-   dateofbirth DATE,
-   gender VARCHAR(10) NOT NULL,
-   color VARCHAR(50) NOT NULL,
-   ownerid INT,foreign key (ownerid) references owners(ownerid));
-
-   CREATE TABLE Appointments (
-    appointid INT PRIMARY KEY,
-    animalid INT,
-    appointdate DATE,
-    reason VARCHAR(255),
-    FOREIGN KEY (animalid) REFERENCES Animals(animalid)
-CREATE TABLE Doctors (
-    doctorid INT PRIMARY KEY,
-    dfirstname VARCHAR(50),
-    dlastname VARCHAR(50),
-    speciality VARCHAR(100),
-    phone VARCHAR(15),
-    email VARCHAR(100)
-);
-CREATE TABLE invoices(
-   invoiceid INT PRIMARY KEY,
-   paymentdate TIME,
-   totalamount NUMERIC(10,2) NOT NULL,
-   appointid INT,
-   foreign key (appointid) references appointments(appointid));
    
    CREATE TABLE MedicalRecords (
     recordid INT PRIMARY KEY,
@@ -51,3 +12,15 @@ CREATE TABLE invoices(
     FOREIGN KEY (animalid) REFERENCES Animals(animalid),
     FOREIGN KEY (doctorid) REFERENCES doctors(doctorid)
 );
+INSERT INTO medicalrecords (recordid, animalid, recorddate, doctorid, diagnosis, prescription, notes)
+VALUES 
+    (1, 1, '2023-01-05 00:00:00', 1, 'Health check', 'N/A', 'Regular checkup, no issue detected'),
+    (2, 2, '2023-01-10 00:00:00', 1, 'Vaccination', 'Vaccine X', 'Administered vaccination X as per schedule'),
+    (3, 3, '2023-02-02 00:00:00', 3, 'Sprained leg', 'Pain Medication', 'Rest recommended for two weeks'),
+    (4, 4, '2023-02-15 00:00:00', 1, 'Dental cleaning', 'N/A', 'Completed dental cleaning procedure'),
+    (5, 5, '2023-03-10 00:00:00', 4, 'Skin infection', 'Antibiotics', 'Prescribed antibiotics for skin infection'),
+    (6, 6, '2023-03-10 00:00:00', 2, 'Flea infestation', 'Flea Treatment', 'Administered flea treatment'),
+    (7, 7, '2023-04-12 00:00:00', 1, 'Vaccination', 'Vaccine Y', 'Administered vaccination Y as per schedule'),
+    (8, 8, '2023-04-18 00:00:00', 5, 'Spaying', 'N/A', 'Successfully performed spaying surgery'),
+    (9, 9, '2023-05-02 00:00:00', 4, 'Allergic reaction', 'Antihistamines', 'Allergic reaction due to food prescribed antihistamine'),
+    (10, 10, '2023-05-20 00:00:00', 6, 'Conjunctivitis', 'Eye drops', 'Prescribed eye drops for conjunctivitis');
